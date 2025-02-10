13d7ea6bbdadb3ff105d786d8e439f1c972a314e
ozlemkayasaroglu <ozlemkayasaroglu@gmail.com>
2025-02-10 20:29:13 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,33 @@
+import express from "express";
+import dotenv from "dotenv";
+import axios from "axios";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+
+app.get("/weather", async (req, res) => {
+  const city = req.query.city;
+  if (!city) {
+    return res.status(400).json({ error: "Lütfen bir şehir adı girin." });
+  }
+
+  const API_KEY = process.env.WEATHER_API_KEY;
+  const API_URL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${API_KEY}`;
+
+  try {
+    console.log("API isteği yapılıyor:", API_URL);
+    const response = await axios.get(API_URL);
+    console.log("API yanıtı:", response.data);
+    res.json(response.data);
+  } catch (error) {
+    console.error("API hatası:", error.response?.data || error.message);
+    res.status(500).json({ error: "Hava durumu verisi alınamadı." });
+  }
+});
+
+app.listen(PORT, () => console.log(`Server ${PORT} portunda çalışıyor`));
